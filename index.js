a9375e276dacfb141ef56c8754981fa5f86e9439
cdubles <33189090+cdubles@users.noreply.github.com>
2021-02-04 15:35:32 -0500

    install packages

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const SQL = require('sqlite3')
 const app = express();
-
+const server = require("http").createServer(app);
 const PORT = 8080;
 
+function openDatabase() {
+    //opens a new database connection
+    let dataBase = new SQL.Database("database.db", function(err) {
+        if (err) return err;
+        console.log("connected to Database");
+    });
+    return dataBase;
+}
+
+app.post('getPosts',(req,res)=>{
+
+})
+
+app.post('getUsers',(req,res)=>{
+
+})
+
 app.use(express.static("public"))
-app.listen(PORT)
+server.listen(PORT)
